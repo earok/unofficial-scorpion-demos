77c6a650183d08c6afdb7e9dc4d82e153a2d017d
Erik Hogan <earok3d@gmail.com>
2023-08-20 23:19:19 +1200

    Latest update

---
+++ cadillacs_and_dinosaurs_aga_megadrive/Actors.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="actors" tilewidth="16" tileheight="16" tilecount="256" columns="0">
+  <image source="actors.png" width="256" height="256" />
+  <tile id="0">
+    <properties>
+      <property name="Name" value="0_stand_hannah" type="string" />
+    </properties>
+  </tile>
+  <tile id="1" />
+  <tile id="2">
+    <properties>
+      <property name="Name" value="0_atk_hannah_0" type="string" />
+    </properties>
+  </tile>
+  <tile id="3">
+    <properties>
+      <property name="Name" value="0_atk_hannah_1" type="string" />
+    </properties>
+  </tile>
+  <tile id="4">
+    <properties>
+      <property name="Name" value="0_atk_hannah_2" type="string" />
+    </properties>
+  </tile>
+  <tile id="5">
+    <properties>
+      <property name="Name" value="gneiss_stand" type="string" />
+    </properties>
+  </tile>
+  <tile id="6">
+    <properties>
+      <property name="Name" value="gneiss_walk" type="string" />
+    </properties>
+  </tile>
+  <tile id="7">
+    <properties>
+      <property name="Name" value="aga_background" type="string" />
+    </properties>
+  </tile>
+</tileset>
---
+++ cadillacs_and_dinosaurs_aga_megadrive/Blocks.tsx
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="blocks" tilewidth="16" tileheight="16" tilecount="256" columns="0">
+  <image source="blocks.png" width="256" height="256" />
+  <tile id="0" />
+  <tile id="1" />
+  <tile id="2" />
+  <tile id="3" />
+  <tile id="4" />
+  <tile id="5" />
+  <tile id="6" />
+  <tile id="7" />
+  <tile id="8" />
+  <tile id="9" />
+  <tile id="10" />
+  <tile id="11" />
+  <tile id="12">
+    <properties>
+      <property name="Name" value="wall" type="string" />
+    </properties>
+  </tile>
+</tileset>
