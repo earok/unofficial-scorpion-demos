7698c9040ab0e04b09b722b65bcbd0015c5a824e
Erik Hogan <earok3d@gmail.com>
2024-01-08 23:57:46 +1300

    Update for 2023.5

---
+++ contra_cd32/Template.tsx
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="templates" tilewidth="256" tileheight="256" tilecount="256" columns="0">
+  <image source="templates.png" width="256" height="256" />
+  <tile id="0">
+    <properties>
+      <property name="Name" value="00_dummycamera" type="string" />
+    </properties>
+  </tile>
+  <tile id="1">
+    <properties>
+      <property name="Name" value="00_dummyactor" type="string" />
+    </properties>
+  </tile>
+  <tile id="2">
+    <properties>
+      <property name="Name" value="00_dummyplayer" type="string" />
+    </properties>
+  </tile>
+  <tile id="3">
+    <properties>
+      <property name="Name" value="01_enmy_01_run" type="string" />
+    </properties>
+  </tile>
+  <tile id="4">
+    <properties>
+      <property name="Name" value="01_turret_grnd" type="string" />
+    </properties>
+  </tile>
+  <tile id="5" />
+  <tile id="6" />
+  <tile id="7" />
+  <tile id="8" />
+  <tile id="9" />
+  <tile id="10" />
+  <tile id="11" />
+  <tile id="12" />
+  <tile id="13" />
+  <tile id="14" />
+  <tile id="15" />
+  <tile id="16">
+    <properties>
+      <property name="Name" value="01_brdg_sct_1" type="string" />
+    </properties>
+  </tile>
+  <tile id="17">
+    <properties>
+      <property name="Name" value="01_brdg_sct_2" type="string" />
+    </properties>
+  </tile>
+  <tile id="18">
+    <properties>
+      <property name="Name" value="01_brdg_sct_3" type="string" />
+    </properties>
+  </tile>
+  <tile id="19">
+    <properties>
+      <property name="Name" value="01_brdg_sct_4" type="string" />
+    </properties>
+  </tile>
+  <tile id="20">
+    <properties>
+      <property name="Name" value="00_expl_01" type="string" />
+    </properties>
+  </tile>
+  <tile id="21">
+    <properties>
+      <property name="Name" value="00_expl_02" type="string" />
+    </properties>
+  </tile>
+  <tile id="22">
+    <properties>
+      <property name="Name" value="01_mountain_bg" type="string" />
+    </properties>
+  </tile>
+  <tile id="23">
+    <properties>
+      <property name="Name" value="00_weap_pod_1" type="string" />
+    </properties>
+  </tile>
+  <tile id="24">
+    <properties>
+      <property name="Name" value="00_weap_pod_2_move_r" type="string" />
+    </properties>
+  </tile>
+  <tile id="25">
+    <properties>
+      <property name="Name" value="00_weap_pod_2_move_l" type="string" />
+    </properties>
+  </tile>
+  <tile id="26">
+    <properties>
+      <property name="Name" value="01_turret_rck" type="string" />
+    </properties>
+  </tile>
+  <tile id="27">
+    <properties>
+      <property name="Name" value="01_reactor" type="string" />
+    </properties>
+  </tile>
+  <tile id="28">
+    <properties>
+      <property name="Name" value="00_flm_up" type="string" />
+    </properties>
+  </tile>
+</tileset>
