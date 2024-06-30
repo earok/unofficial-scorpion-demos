7dc2940ce542b382e1c8c2dcf7ead7d77e3b1d92
Erik Hogan <earok3d@gmail.com>
2024-06-30 21:24:58 +1200

    .

---
+++ lost_vikings_2/lv2_scorpion/Blocks.tsx
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="blocks" tilewidth="16" tileheight="16" tilecount="256" columns="0">
+  <image source="blocks.png" width="256" height="256" />
+  <tile id="0">
+    <properties>
+      <property name="Name" value="slope_solid" type="string" />
+    </properties>
+  </tile>
+  <tile id="1">
+    <properties>
+      <property name="Name" value="slope_d1" type="string" />
+    </properties>
+  </tile>
+  <tile id="2">
+    <properties>
+      <property name="Name" value="slope_d2" type="string" />
+    </properties>
+  </tile>
+  <tile id="3">
+    <properties>
+      <property name="Name" value="slope_d3" type="string" />
+    </properties>
+  </tile>
+  <tile id="4">
+    <properties>
+      <property name="Name" value="slope_u1" type="string" />
+    </properties>
+  </tile>
+  <tile id="5">
+    <properties>
+      <property name="Name" value="slope_u2" type="string" />
+    </properties>
+  </tile>
+  <tile id="6">
+    <properties>
+      <property name="Name" value="slope_u3" type="string" />
+    </properties>
+  </tile>
+</tileset>
