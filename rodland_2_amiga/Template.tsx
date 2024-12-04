197e4a107b297d902cde09a3d1f9cfb7e404a962
Erik Hogan <earok3d@gmail.com>
2024-12-04 17:48:48 +1300

    Rodland 2

---
+++ rodland_2_amiga/Template.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="templates" tilewidth="256" tileheight="256" tilecount="256" columns="0">
+  <image source="templates.png" width="256" height="256" />
+  <tile id="0">
+    <properties>
+      <property name="Name" value="p1" type="string" />
+    </properties>
+  </tile>
+  <tile id="1">
+    <properties>
+      <property name="Name" value="p1_climb" type="string" />
+    </properties>
+  </tile>
+  <tile id="2">
+    <properties>
+      <property name="Name" value="p1_ladder" type="string" />
+    </properties>
+  </tile>
+  <tile id="3">
+    <properties>
+      <property name="Name" value="misc" type="string" />
+    </properties>
+  </tile>
+  <tile id="4">
+    <properties>
+      <property name="Name" value="p1_sword" type="string" />
+    </properties>
+  </tile>
+</tileset>
